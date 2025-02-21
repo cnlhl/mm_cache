49c08cf8966fd2a42e9420f312f34942efa36409
mxLi <mddlhl@hotmail.com>
2025-02-21 18:15:31 +0800

    Refactor Level2Engine to improve data retrieval and integrity checks, replacing data_cache with direct date parameter in calculations

--- level2engine.py
+++ level2engine.py
@@ -36,13 +36,13 @@ class Level2Engine:
         return self._cache_client.check(date_only=True)
         
     
-    def _calculate(self, code_, data_cache):
+    def _calculate(self, code_, date_):
         try:
             s_t = time.time()
+            data_cache = {t: self._cache_client.get(t, date_, code_) for t in self._data_types}
             res = self.on_calculate(data_cache)
             if len(res) > 0:
                 res[self._code_column] = int(code_)
-            # logging.warning('calc {} at {} in {}s'.format(code_, date_, time.time() - s_t))
             return res
         except Exception as e_msg:
             return pd.DataFrame()
@@ -63,33 +63,19 @@ class Level2Engine:
 
                     s_t = time.time()
                     codes = self._map.get(d)
-                    
-                    # 预先获取所有股票的数据并检查完整性
-                    s_t_1 = time.time()
-                    data_caches = {}
-                    valid_codes = []
-                    for code in codes:
-                        # to_do: 这里的type逻辑和循环外的type逻辑冲突了
-                        data_cache = {type: self._cache_client.get(type, d, code) for type in self._data_types}
-                        # 检查是否有任何数据为None或者为空
-                        if all(data is not None and not data.empty for data in data_cache.values()):
-                            data_caches[code] = data_cache
-                            valid_codes.append(code)
-                    
-                    # 检查数据完整性
-                    completion_rate = len(valid_codes) / len(codes)
-                    if completion_rate <= 0.8:  # 如果完整率低于80%，跳过该日期
-                        logging.warning(f'数据完整率过低 {d}: {completion_rate:.2%}, 跳过处理')
-                        continue
+                    cached_ids = self._cache_client.check(type, date_only=False)
+                    cached_d_ids = [c for c in cached_ids if d in c]
 
-                    logging.warning(f'fetch data for {d} in {time.time() - s_t_1:.2f}s')
+                    if len(cached_d_ids)/len(codes) < 0.8:
+                        logging.warning(f'{d} is not ready')
+                        continue
                     
                     s_t_1 = time.time()
 
                     if core <= 0:
-                        res_d = pd.concat([self._calculate(code, data_caches[code]) for code in valid_codes])
+                        res_d = pd.concat([self._calculate(code,d) for code in codes])
                     else:
-                        calc_params = [(code, data_caches[code]) for code in valid_codes]
+                        calc_params = [(code, d) for code in codes]
                         map_res = pool.starmap(self._calculate, calc_params)
                         res_d = pd.concat(map_res)
                         del map_res
